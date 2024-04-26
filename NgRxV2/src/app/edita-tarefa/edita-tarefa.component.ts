7c82a50f3211df4a14bde780635f487fdb16de47
ValberF <valberfrsantos@gmail.com>
2024-04-25 23:38:11 -0300

    FEA-P011

---
+++ NgRxV2/src/app/edita-tarefa/edita-tarefa.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-edita-tarefa',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './edita-tarefa.component.html',
+  styleUrls: ['./edita-tarefa.component.css']
+})
+export class EditaTarefaComponent {
+
+}
